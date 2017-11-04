2896b3ecf31affd846753fac712ccd5857c326bd
shymaSane <shymaSane1@gmail.com>
2017-11-04 20:37:56 +0200

    trying to fix output

--- Output/src/app/child/child.component.ts
+++ Output/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
+  @Input() name: string;
+  @Output() onVoted = new EventEmitter<boolean>()
+  voted = false;
+
+  vote(agreed:boolean){
+    this.onVoted.emit(agreed)
+    this.voted = true
+  }
+
 
   constructor() { }
 
